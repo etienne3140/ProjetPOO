bdec39d3576f2f10fcf578f77e871e478b2ae621
Etienne <etienne.blaye@phelma.grenoble-inp.fr>
2023-10-31 15:13:57 +0100

    J'ai regroupé les classes CPU, registre et programme dans le fichier CPU.hpp pour éviter les problèmes d'inclusion, ajout de la méthode compute pour le programme

--- include/CPU.hpp
+++ include/CPU.hpp
@@ -2,22 +2,101 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <queue>
 
 using namespace std;
 
 #ifndef CPU_H
 #define CPU_H
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~Création du registre~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+class REGISTER {
+    private :
+        double value;
+    public :
+        queue<double> fifo;
+        void AddResult(double result);
+        double ReturnResult();
+        REGISTER() :
+            value(0)
+            {};
+};
+
+void REGISTER::AddResult(double result){
+    fifo.push(result);
+}
+
+double REGISTER::ReturnResult(){
+    if (!fifo.empty()) {
+        double result = fifo.front();
+        fifo.pop();
+        return result;
+    }
+    return 0;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~Création du programme~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class PROGRAMME {
+    private :
+        ifstream file;
+    public :
+        string program;
+        void load();
+        double compute();
+        PROGRAMME() :
+            program(""),
+            file("data/"+program)
+            {};
+};
+
+void PROGRAMME::load(){
+    REGISTER registre;
+
+}
+
+double PROGRAMME::compute(){
+    if (!file) {
+        cerr << "Impossible d'ouvrir le fichier " << program << endl;
+        return 0;
+    }
+    string ope, nb1, nb2;
+    file >> ope >> nb1 >> nb2;
+    if (ope == "NOP"){
+        return 0;
+    }
+    else if (ope == "ADD"){
+        return stod(nb1)+stod(nb2);
+    }
+    else if(ope == "SUB"){
+        return stod(nb1)-stod(nb2);
+    }
+    else if(ope == "MUL"){
+        return stod(nb1)*stod(nb2);
+    }
+    else if (ope == "DIV"){
+        return stod(nb1)/stod(nb2);
+    }
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~Création du CPU~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class CPU {
     private:
         string LABEL;
         int FREQUENCY;
         int CORES;
+        string PROGRAM;
         int CurrentlyActiveCores;
     public:
-        string PROGRAM;
         void load(const string& fileName);      
         void PrintCPU();
         CPU() :
@@ -31,32 +110,33 @@ class CPU {
 
 
 void CPU::load(const string& fileName) {
-        ifstream file("data/"+fileName);
-
-        if (!file) {
-            cerr << "Impossible d'ouvrir le file " << fileName << endl;
-            return;
+    PROGRAMME program;
+    program.load();
+    ifstream file("data/"+fileName);
+    if (!file) {
+        cerr << "Impossible d'ouvrir le file " << fileName << endl;
+        return;
+    }
+    string line;
+    while (getline(file, line)) {
+        size_t pos = line.find(':');
+        string cle = line.substr(0, pos);
+        string value = line.substr(pos + 2); 
+        if (cle == "LABEL") {                    
+            LABEL = value;
         }
-
-        string line;
-        while (getline(file, line)) {
-            size_t pos = line.find(':');
-            string cle = line.substr(0, pos);
-            string value = line.substr(pos + 2); 
-            if (cle == "LABEL") {                    
-                LABEL = value;
-            }
-            else if (cle == "FREQUENCY") {
-                FREQUENCY = stoi(value); 
-            } 
-            else if (cle == "CORES") {
-                CORES = stoi(value); 
-            }
-            else if (cle == "PROGRAM"){
-                PROGRAM = value;
-            }
+        else if (cle == "FREQUENCY") {
+            FREQUENCY = stoi(value); 
+        } 
+        else if (cle == "CORES") {
+            CORES = stoi(value); 
         }
-        file.close();
+        else if (cle == "PROGRAM"){
+            PROGRAM = value;
+            program.program = value; //Ajout nom du fichier program dans la classe PROGRAM
+        }
+    }
+    file.close();
 }
 
 
--- src/projet.cpp
+++ src/projet.cpp
@@ -3,21 +3,26 @@
 #include "BUS.hpp"
 #include "MEMORY.hpp"
 #include "DISPLAY.hpp"
-#include "REGISTER.hpp"
+
+
+
+ifstream file("data/program.txt");
+
+void computing(){
+    if (!file) {
+        cerr << "Impossible d'ouvrir le fichier " << "program.txt" << endl;
+        return;
+    }
+    string ope, nb1, nb2;
+    file >> ope >> nb1 >> nb2;
+    cout << ope << endl;
+    cout << nb1 << endl;
+    cout << nb2 << endl;
+}
 
 int main(){
-    REGISTER registre;
-    
-    registre.AddResult(12.53);
-    registre.AddResult(1.62);
-    registre.AddResult(543.482);
-
-    while (!registre.fifo.empty()) {
-        double resultat = registre.ReturnResult();
-        cout << "Résultat du registre : " << resultat << endl;
-    }
-
-    return 0;
+    computing();
+    computing();
 }
 
 
